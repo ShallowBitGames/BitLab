13e09a5e0086a2ca8d80e3fe1917e1fcb577ebf2
Lukas <dana.lenz@protonmail.com>
2025-01-21 15:38:17 +0100

    recipe

---
+++ BitLabyrinth/Recipe/Recipe.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitLabyrinth.RecipeTree
+{
+
+    internal struct Requirement<ID> where ID : IEquatable<ID>
+    {
+        ID ingredient;
+        int min_required;
+        int max_optional;
+
+        public Requirement(ID ingredient, int min_required, int max_optional)
+        {
+            this.ingredient = ingredient;
+            this.min_required = min_required;
+            this.max_optional = max_optional;
+        }
+
+        bool isOptional() { return min_required == 0; }
+
+        bool fulfils_required(ID ing, int amount) { return Equals(ing, ingredient) && amount >= min_required; }
+
+        bool fulfils_optional(ID ing, int amount) { return Equals(ing, ingredient) && (max_optional == -1 || amount < max_optional); }
+
+    }
+
+    public class Recipe<ID> where ID : IEquatable<ID> {
+        
+    //    Requirement<ID>[] requirements = [];
+
+    
+    }
+
+    bool operator<(Recipe lh, Recipe rh) 
+    {
+            bool smaller_reqs = false;
+            bool larger_reqs = false;
+            bool inconclusive = false;
+            
+            for()
+    }
+
+        public static bool operator <(Recipe lh, Recipe rh)
+        {
+
+        }
+    }
---
+++ BitLabyrinth/Recipe/RecipeTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitLabyrinth.Recipe
+{
+    internal class RecipeTree
+    {
+
+        //
+
+        //
+
+        public bool AddNode(Recipe rec) { 
+        
+            // exact match ==: return false
+
+            // if < than some node: 
+
+            // if > than some node: gather
+
+            // if sameSpot as some node: return false
+
+            // if 
+
+            // gather nodes of current level that are either
+            // <, >, <>
+
+            // < recipes: become children of new node
+            // >
+        
+        }
+
+    }
+}
