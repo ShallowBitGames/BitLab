a1b63b05d9fc779b5c5e7f0976eac3cd3e27d280
Lukas <LenzLukas@proton.me>
2025-02-14 12:30:02 +0100

    fixed comparison logic

--- BitLabyrinth/Recipe/Recipe.cs
+++ BitLabyrinth/Recipe/Recipe.cs
@@ -46,26 +46,35 @@
 
         // a is smaller than b iff
         // requirements of a fulfilled => requirements of b fulfilled
+        // check if each of b's requirements fits into a's requirements
         public static bool operator <(Recipe<ID> lh, Recipe<ID> rh)
         {
 
-            List<Requirement<ID>> remainingRH = new(rh.Requirements);
+            List<Requirement<ID>> remainingLH = new(lh.Requirements);
 
-            foreach (Requirement<ID> requiredLH in lh.Requirements) {
+            foreach (Requirement<ID> required_rh in rh.Requirements) {
 
-                int matchID = remainingRH.FindIndex(requiredRH => EqualityComparer<ID>.Default.Equals(requiredLH.ingredient, requiredRH.ingredient));
-                if (requiredLH.min_required > 0 && matchID == -1)
+                int matchID = remainingLH.FindIndex(required_lh => EqualityComparer<ID>.Default.Equals(required_rh.ingredient, required_lh.ingredient));
+                if ( matchID == -1 )
+                {
+                    if (required_rh.min_required > 0)
+                        return false;
+
+                    // add a new matching requirement
+                    remainingLH.Add(required_rh);
+                    matchID = remainingLH.IndexOf(required_rh);
+
+                }
+
+                var match = remainingLH[matchID];
+
+                if (match.min_required < required_rh.min_required)
                     return false;
 
-                var match = remainingRH[matchID];
+                match.min_required += required_rh.min_required;
+                match.max_optional -= required_rh.max_optional;
 
-                if (match.min_required > requiredLH.min_required)
-                    return false;
-
-                match.min_required += requiredLH.min_required;
-                match.max_optional -= requiredLH.max_optional;
-
-                remainingRH[matchID] = match;
+                remainingLH[matchID] = match;
 
                 }
 
